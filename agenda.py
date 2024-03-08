74f0cc0a4520629b64523256dfe75e9c754a3dbb
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2024-03-08 20:30:05 -0300

    Melhorando metodo exibir
    Mehorando metdo de exibir agenda

--- agenda.py
+++ agenda.py
@@ -1,4 +1,8 @@
-AGENDA = {
+AGENDA = {'jonas':{
+    'tel':'13-997839576',
+    'email': 'jonas@email.com',
+    'endereco': 'av 1',
+}
 
 }
 
@@ -7,7 +11,12 @@ def exibir_agenda():
     if len(AGENDA) == 0:
         print('Nenhum Contato Cadastrado')
     else:
-        print(AGENDA)
+        for contato in AGENDA:
+            print("Nome:", contato)
+            print("Telefone:", AGENDA[contato]['tel'])
+            print("Email:", AGENDA[contato]['email'])
+            print("Endereço:", AGENDA[contato]['endereco'])
+            print('-' * 30)
 
 
 exibir_agenda()
