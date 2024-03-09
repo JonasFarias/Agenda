36ef9f49108935892cf7feae6ff5a5d6532328df
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2024-03-08 23:36:16 -0300

    Criando menu CLI
    criando método do menu AKA

--- agenda.py
+++ agenda.py
@@ -32,3 +32,46 @@ def inclur_editar_contato(nome, telefone, email, endereco):
     }
     print(f'--> Contato {nome}, adicionado com sucesso!')
 
+def excluir_contato(contato):
+    AGENDA.pop(contato)
+    print(f'{contato}, excluido com sucesso!')
+
+
+def opcoes_menu():
+    opcao = int(input('Qual  Opção: '))
+    if opcao == 1:
+        exibir_agenda()
+    elif opcao == 2:
+        contato = input('Buscar: ')
+        buscar_contato(contato)
+    elif opcao == 3 or 4:
+        nome = input('Nome: ')
+        telefone = input('Telefone: ')
+        email = input('Email: ')
+        endereco = input('Endereço: ')
+        inclur_editar_contato(nome, telefone, email, endereco)
+    elif opcao == 4:
+        inclur_editar_contato()
+    elif opcao == 5:
+        contato = input('Contato: ')
+        excluir_contato(contato)
+    elif opcao == 0:
+        pass
+
+def exibir_menu():
+    while True:
+        print('\n')
+        print('1 - EXIBIR TODOS CONTATOS')
+        print('2 - BUSCAR CONTATO')
+        print('3 - ADICIONAR CONTATO')
+        print('4 - EDITAR CONTATO')
+        print('5 - EXCLUIR CONTATO')
+        print('0 - FECHAR AGENDA')
+        opcoes_menu()
+
+
+
+
+
+exibir_menu()
+opcoes_menu()
